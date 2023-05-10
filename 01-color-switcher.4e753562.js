e30bfd956545c6fd55b1252e200fbdbbe3766218
rafalbarczak <117038018+rafalbarczak@users.noreply.github.com>
2023-05-10 01:15:48 +0000

    Deploying to gh-pages from @ rafalbarczak/goit-parcel-repository@cdc6a571097818563ff105d0f79d1fe26aaf7813 🚀

---
+++ 01-color-switcher.4e753562.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){e.setAttribute("disabled","true"),changeColor=setInterval((function(){t.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3)})),o.addEventListener("click",(function(){e.removeAttribute("disabled"),clearInterval(changeColor)}))}();
+//# sourceMappingURL=01-color-switcher.4e753562.js.map
---
+++ 01-color-switcher.c2231c5e.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");e.addEventListener("click",(()=>{e.setAttribute("disabled","true"),changeColor=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),o.addEventListener("click",(()=>{e.removeAttribute("disabled"),clearInterval(changeColor)}));
+//# sourceMappingURL=01-color-switcher.c2231c5e.js.map
